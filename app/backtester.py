882e3f78549eb0623093478bbb408bd248059261
Simon <spam@esemi.ru>
2023-07-25 22:01:09 +0200

    :sparkles: logging

--- app/backtester.py
+++ app/backtester.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from app.models import Position, OnHoldPositions
@@ -11,7 +12,7 @@ def main() -> None:
     max_onhold_positions: OnHoldPositions | None = None
 
     for tick_number, tick_rate in enumerate(history_rates):
-        print('tick', tick_number, tick_rate)
+        logging.debug('tick {0} {1}'.format(tick_number, tick_rate))
 
         on_hold_current = OnHoldPositions(
             amount=sum([pos.amount for pos in open_positions]),
@@ -22,7 +23,7 @@ def main() -> None:
             max_onhold_positions = on_hold_current
 
         if not tick_number:
-            print('init buy')
+            logging.info('init buy')
             for _ in range(app_settings.init_buy_amount):
                 open_positions.append(Position(
                     amount=app_settings.continue_buy_amount,
@@ -32,51 +33,54 @@ def main() -> None:
             continue
 
         if tick_number == 1:
-            print('skip')
+            logging.debug('skip')
             continue
 
         # check global stop loss
         if tick_rate <= app_settings.global_stop_loss:
-            print('global stop loss fired!', len(open_positions), len(closed_positions))
+            logging.warning('global stop loss fired! open: {0}. closed: {1}'.format(
+                len(open_positions),
+                len(closed_positions),
+            ))
             closed_positions = _close_all(open_positions, closed_positions, tick_rate, tick_number)
             open_positions = []
             break
 
         # search position for sale
         avg_rate: float = (history_rates[tick_number - 2] + history_rates[tick_number - 1]) / 2
-        print('search position for sell. Avg rate: {0}, tick rate: {1}'.format(avg_rate, tick_rate))
+        logging.debug('search position for sell. Avg rate: {0}, tick rate: {1}'.format(avg_rate, tick_rate))
 
         sale_completed: bool = False
         for open_position_index, position in enumerate(open_positions):
-            print(position)
+            logging.debug(position)
 
             # условия на продажу
             # - средняя превысила цену покупки на 5%
-            print('check sale by avg rate and open rate. Open rate +5% {0}. Average rate +5% {1}'.format(
+            logging.debug('check sale by avg rate and open rate. Open rate +5% {0}. Average rate +5% {1}'.format(
                 position.open_rate * app_settings.avg_rate_sell_limit,
                 avg_rate * app_settings.avg_rate_sell_limit,
             ))
             if avg_rate >= position.open_rate * app_settings.avg_rate_sell_limit:
                 # - текущая цена выше чем средняя +5%
                 if tick_rate >= avg_rate * app_settings.avg_rate_sell_limit:
-                    print('close position')
                     sale_position = open_positions.pop(open_position_index)
                     sale_position.close_rate = tick_rate
                     sale_position.close_tick_number = tick_number
                     closed_positions.append(sale_position)
+                    logging.info('close position {0}'.format(sale_position))
                     sale_completed = True
                     break
 
             # - текущая цена выше цены покупки на 0.02+5%
-            print('check sale by tick rate and open rate. Open rate + step + 5%: {0}'.format(
+            logging.debug('check sale by tick rate and open rate. Open rate + step + 5%: {0}'.format(
                 position.open_rate * app_settings.avg_rate_sell_limit + app_settings.step,
             ))
             if tick_rate >= position.open_rate * app_settings.avg_rate_sell_limit + app_settings.step:
-                print('close position')
                 sale_position = open_positions.pop(open_position_index)
                 sale_position.close_rate = tick_rate
                 sale_position.close_tick_number = tick_number
                 closed_positions.append(sale_position)
+                logging.info('close position {0}'.format(sale_position))
                 sale_completed = True
                 break
 
@@ -85,18 +89,18 @@ def main() -> None:
 
         # вот смотри там где разница была больше или равно 0.02 мы закупали
         rate_go_down = history_rates[tick_number - 1] - tick_rate
-        print('check rates for buy. Prev rate: {0}, diff {1}'.format(
+        logging.debug('check rates for buy. Prev rate: {0}, diff {1}'.format(
             history_rates[tick_number - 1],
             rate_go_down,
         ))
         if rate_go_down >= app_settings.step:
-            print('open position')
+            logging.info('open new position')
             open_positions.append(Position(
                 amount=app_settings.continue_buy_amount,
                 open_rate=tick_rate,
                 open_tick_number=tick_number,
             ))
-
+    
     _show_results(closed_positions, open_positions, last_rate=history_rates[-1], onhold=max_onhold_positions)
 
 
@@ -135,19 +139,25 @@ def _show_results(
         [last_rate * pos.amount for pos in open_positions]
     )
 
-    print('open positions', len(open_positions))
-    print('closed positions', len(closed_positions))
-    print(f'потратили на покупки монет {buy_amount}$')
-    print(f'получили монет с продажи монет {sell_amount}$')
-    print(f'сумма за ликвидацию зависших монет {hold_amount}$')
+    print('')
+    print('Результаты тестирования:')
+    print(f'открытых позиций на конец торгов {len(open_positions)}')
+    print(f'закрытых позиций на конец торгов {len(closed_positions)}')
+    print('')
+    print('потратили на покупки монет $%.2f' % buy_amount)
+    print('получили монет с продажи монет $%.2f' % sell_amount)
+    print('сумма за ликвидацию зависших монет $%.2f' % hold_amount)
+    print('')
 
     buy_amount = buy_amount or 1.0
-    print(f'доходность без учёта зависших монет: {(sell_amount - buy_amount) / buy_amount * 100}%')
-    print(f'доходность без учёта зависших монет: {sell_amount - buy_amount}$')
-    print(f'доходность с учётом зависших монет: {(sell_amount + hold_amount - buy_amount) / buy_amount * 100}%')
-    print(f'доходность с учётом зависших монет: {sell_amount + hold_amount - buy_amount}$')
+    print('доходность без учёта зависших монет: %.2f%%' % ((sell_amount - buy_amount) / buy_amount * 100))
+    print('доходность без учёта зависших монет: $%.2f' % (sell_amount - buy_amount))
+    print('')
+    print('доходность с учётом зависших монет: %.2f%%' % ((sell_amount + hold_amount - buy_amount) / buy_amount * 100))
+    print('доходность с учётом зависших монет: $%.2f' % (sell_amount + hold_amount - buy_amount))
 
     if onhold:
+        print('')
         print(f'максимум монет на руках: {onhold.amount} монет на тике {onhold.tick_number} (курс {onhold.rate})')
 
 
@@ -165,4 +175,8 @@ def _close_all(
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG if app_settings.debug else logging.INFO,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+    )
     main()
--- app/settings.py
+++ app/settings.py
@@ -8,6 +8,7 @@ from pydantic_settings import BaseSettings
 class AppSettings(BaseSettings):
     """Application settings class."""
 
+    debug: bool = Field(default=False)
     step: float = Field(default=0.02, description='шаг в абсолютных значениях для условия сделок')
     avg_rate_sell_limit: float = Field(default=1.05, description='шаг в процентах для условия сделок. 5% = 1.05')
     init_buy_amount: int = Field(default=3, description='сколько позиций открываем в самом начале теста')
