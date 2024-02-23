1e77baa74febc009cd46216665fac22e1467d525
Simon <spam@esemi.ru>
2024-02-23 22:06:19 +0100

    :sparkles: Inprove backtester

--- app/backtester.py
+++ app/backtester.py
@@ -1,5 +1,7 @@
+import argparse
 import logging
 import os
+from datetime import datetime, timezone
 from decimal import Decimal
 
 from app.exchange_client.dummy import Dummy
@@ -10,14 +12,23 @@ from app.strategy import get_strategy_instance
 logger = logging.getLogger(__name__)
 
 
-def main() -> None:
+def main(
+    use_every_n_tick: int = 1,
+    start_date: datetime | None = None,
+    end_date: datetime | None = None,
+) -> None:
     strategy = get_strategy_instance(
         strategy_type=app_settings.strategy_type,
         exchange_client=Dummy(symbol='dummy'),
         dry_run=True,
     )
 
-    for tick in get_rates(app_settings.rates_filename):
+    for tick in get_rates(
+        app_settings.rates_filename,
+        use_every_n_tick,
+        start_date,
+        end_date,
+    ):
         logger.info('tick {0}'.format(tick))
 
         go_to_next_step = strategy.tick(tick=tick)
@@ -28,7 +39,12 @@ def main() -> None:
     strategy.show_results()
 
 
-def get_rates(filename: str) -> list[Tick]:
+def get_rates(
+    filename: str,
+    use_every_n_tick: int = 1,
+    start_date: datetime | None = None,
+    end_date: datetime | None = None,
+) -> list[Tick]:
     filepath = os.path.abspath(os.path.join(
         app_settings.rates_path,
         filename,
@@ -40,6 +56,14 @@ def get_rates(filename: str) -> list[Tick]:
         for num, line in enumerate(fd):
             if not num or not line:
                 continue
+
+            if num % use_every_n_tick:
+                continue
+
+            if start_date and end_date:
+                tick_date = datetime.fromisoformat(line.split(',')[0]).replace(tzinfo=timezone.utc)
+                if tick_date < start_date or tick_date > end_date:
+                    continue
 
             price = Decimal(line.split(',')[1])
             output.append(Tick(
@@ -53,10 +77,29 @@ def get_rates(filename: str) -> list[Tick]:
     return output
 
 
+def valid_datetime(s: str) -> datetime:
+    try:
+        return datetime.strptime(s, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+    except ValueError:
+        msg = "Not a valid date: {0!r}".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+
 if __name__ == '__main__':
     logging.basicConfig(
         filename=os.path.join(APP_PATH, 'backtest.log'),
         level=logging.DEBUG if app_settings.debug else logging.INFO,
         format='%(asctime)s %(levelname)-8s %(message)s',
     )
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--from-date', default=None, help='Use ticks from datetime (eg. 2023-01-01 00:00:00)', type=valid_datetime)
+    parser.add_argument('--to-date', default=None, help='Use ticks to datetime (eg. 2023-02-02 23:59:59)', type=valid_datetime)
+    parser.add_argument('--used-ticks', default=1, help='Use every N tick', type=int)
+    args = parser.parse_args()
+
+    main(
+        use_every_n_tick=args.used_ticks,
+        start_date=args.from_date,
+        end_date=args.to_date,
+    )
